012fd580d6327c3b4bacf2abfce649995744b7f0
vegas-casinoo <vegascasinohelps@gmail.com>
2026-02-14 10:11:04 +0300

    Update server.js

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -8,6 +8,14 @@ import { promoRoutes } from "./routes/promo.routes.js";
 import { errorMiddleware } from "./middlewares/error.js";
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://the-gift-cases-staticsite.onrender.com");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
 app.use(express.json());
 
 app.get("/api/health", (req, res) => res.json({ ok: true }));
