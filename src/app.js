092b2946487b379cb6bc6f5b01c1a0bc645cd329
vegas-casinoo <vegascasinohelps@gmail.com>
2026-02-22 15:48:33 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import { initRouter } from "./router/router.js";
 import { renderLayout } from "./ui/layout.js";
 
-const tg = window.Telegram.WebApp;
-tg.ready();
-tg.expand();
+const tg = window.Telegram?.WebApp;
+if (tg) {
+  tg.ready();
+  tg.expand();
+}
+
+// Тест что JS реально запустился
+document.getElementById("app").innerHTML =
+  "<div style='padding:16px;color:#fff'>JS LOADED ✅</div>";
 
 renderLayout();
 initRouter();
